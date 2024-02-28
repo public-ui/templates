017ee72156107111a3a2387541c82c59afb7d9a6
Martin Oppitz <6279703+deleonio@users.noreply.github.com>
2024-02-28 08:23:56 +0100

    chore: initial commit

---
+++ templates/solid-webpack/src/solid.main.tsx
@@ -0,0 +1,15 @@
+import { render } from 'solid-js/web';
+
+import { register } from '@public-ui/components';
+import { defineCustomElements } from '@public-ui/components/dist/loader';
+import { DEFAULT } from '@public-ui/themes';
+import App from './App';
+
+register(DEFAULT, defineCustomElements)
+	.then(() => {
+		const htmlDivElement: HTMLDivElement | null = document.querySelector('div#app');
+		if (htmlDivElement instanceof HTMLDivElement) {
+			render(() => <App />, htmlDivElement);
+		}
+	})
+	.catch(console.warn);
