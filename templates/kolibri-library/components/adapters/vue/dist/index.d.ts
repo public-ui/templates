ad2e48dc528c6f7bec9e2ddfaa6458431dad6188
Martin Oppitz <6279703+deleonio@users.noreply.github.com>
2024-03-06 08:05:14 +0100

    chore: move stuff

--- templates/kolibri-library/components/adapters/vue/dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { JSX } from '@public-ui/library-components';
-
-interface InputProps<T> {
-    modelValue?: T;
-}
-
-declare const DemoButton: (props: JSX.DemoButton & InputProps<string | number | boolean> & {}) => any;
-
-export { DemoButton };
