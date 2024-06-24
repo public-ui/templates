328c9ce7b51a88fa06174be9f4391509088db18b
Stefan Dietz <mail@stefan-dietz.eu>
2024-06-24 10:52:09 +0200

    feat: define JSX strings consistently and add some visual spacing

--- csr/react-vite-formik/src/App.tsx
+++ csr/react-vite-formik/src/App.tsx
@@ -59,13 +59,13 @@ function App() {
 	return (
 		<div className="App mx-auto max-w-800px p-16">
 			{submittedValues && (
-				<div className={'mb-8'}>
-					<KolHeading _label={'Submitted Values'}></KolHeading>
+				<div className="mb-8">
+					<KolHeading _label="Submitted Values"></KolHeading>
 					<pre>{JSON.stringify(submittedValues, null, 2)}</pre>
 				</div>
 			)}
 
-			<KolHeading _label={'Form'}></KolHeading>
+			<KolHeading _label="Form"></KolHeading>
 
 			<Formik
 				initialValues={initialValues}
@@ -81,95 +81,97 @@ function App() {
 							},
 						}}
 					>
-						<KolInputText _label={'Text'} _value={values.inputText} _name={'inputText'} _on={{ onInput: handleChange }} />
-						<KolInputCheckbox
-							_label={'Checkbox'}
-							_name={'inputCheckbox'}
-							_on={{
-								onInput: (event, value) => {
-									setValues({
-										...values,
-										inputCheckbox: value,
-									});
-								},
-							}}
-						/>
-						<KolInputColor _label={'Color'} _value={values.inputColor} _name={'inputColor'} _on={{ onInput: handleChange }} />
-						<KolInputDate _label={'Date'} _value={values.inputDate} _name={'inputDate'} _on={{ onInput: handleChange }} />
-						<KolInputEmail _label={'Email'} _value={values.inputEmail} _name={'inputEmail'} _on={{ onInput: handleChange }} />
-						<KolInputFile
-							_label={'File'}
-							_value={values.inputFile}
-							_name={'inputFile'}
-							_on={{
-								onInput: (event, value) => {
-									setValues({
-										...values,
-										inputFile: value,
-									});
-								},
-							}}
-						/>
-						<KolInputNumber _label={'Number'} _value={values.inputNumber} _name={'inputNumber'} _on={{ onInput: handleChange }} />
-						<KolInputPassword _label={'Password'} _value={values.inputPassword} _name={'inputPassword'} _on={{ onInput: handleChange }} />
-						<KolInputRange
-							_label={'Range'}
-							_value={values.inputRange}
-							_name={'inputRange'}
-							_on={{
-								onInput: (event, value) => {
-									setValues({
-										...values,
-										inputRange: value,
-									});
-								},
-							}}
-						/>
-						<KolInputRadio
-							_label={'Radio'}
-							_orientation="horizontal"
-							_options={[
-								{ label: 'New York', value: 'New York' },
-								{ label: 'Rio de Janeiro', value: 'Rio de Janeiro' },
-								{ label: 'Rosenheim', value: 'Rosenheim' },
-							]}
-							_value={values.inputRadio}
-							_name={'inputRadio'}
-							_on={{
-								onInput: (event, value) => {
-									setValues({
-										...values,
-										inputRadio: value,
-									});
-								},
-							}}
-						/>
-						<KolSelect
-							_label={'Select'}
-							_orientation="horizontal"
-							_options={[
-								{ label: 'New York', value: 'New York' },
-								{ label: 'Rio de Janeiro', value: 'Rio de Janeiro' },
-								{ label: 'Rosenheim', value: 'Rosenheim' },
-							]}
-							_value={values.select}
-							_name={'select'}
-							_on={{
-								onInput: (event, value) => {
-									setValues({
-										...values,
-										select: value,
-									});
-								},
-							}}
-						/>
-						<KolTextarea _label={'Textarea'} _value={values.textarea} _name={'textarea'} _on={{ onInput: handleChange }} />
-						<div className="mt">
-							<KolButton _label={'Button'} _value={values.button} _name={'button'} _type="submit" />
-							<KolButtonLink _label={'ButtonLink'} _value={values.buttonLink} _name={'buttonLink'} _type="submit" />
-						</div>
+						<div className="grid gap-lg">
+							<KolInputText _label="Text" _value={values.inputText} _name="inputText" _on={{ onInput: handleChange }} />
+							<KolInputCheckbox
+								_label="Checkbox"
+								_name="inputCheckbox"
+								_on={{
+									onInput: (event, value) => {
+										setValues({
+											...values,
+											inputCheckbox: value,
+										});
+									},
+								}}
+							/>
+							<KolInputColor _label="Color" _value={values.inputColor} _name="inputColor" _on={{ onInput: handleChange }} />
+							<KolInputDate _label="Date" _value={values.inputDate} _name="inputDate" _on={{ onInput: handleChange }} />
+							<KolInputEmail _label="Email" _value={values.inputEmail} _name="inputEmail" _on={{ onInput: handleChange }} />
+							<KolInputFile
+								_label="File"
+								_value={values.inputFile}
+								_name="inputFile"
+								_on={{
+									onInput: (event, value) => {
+										setValues({
+											...values,
+											inputFile: value,
+										});
+									},
+								}}
+							/>
+							<KolInputNumber _label="Number" _value={values.inputNumber} _name="inputNumber" _on={{ onInput: handleChange }} />
+							<KolInputPassword _label="Password" _value={values.inputPassword} _name="inputPassword" _on={{ onInput: handleChange }} />
+							<KolInputRange
+								_label="Range"
+								_value={values.inputRange}
+								_name="inputRange"
+								_on={{
+									onInput: (event, value) => {
+										setValues({
+											...values,
+											inputRange: value,
+										});
+									},
+								}}
+							/>
+							<KolInputRadio
+								_label="Radio"
+								_orientation="horizontal"
+								_options={[
+									{ label: 'New York', value: 'New York' },
+									{ label: 'Rio de Janeiro', value: 'Rio de Janeiro' },
+									{ label: 'Rosenheim', value: 'Rosenheim' },
+								]}
+								_value={values.inputRadio}
+								_name="inputRadio"
+								_on={{
+									onInput: (event, value) => {
+										setValues({
+											...values,
+											inputRadio: value,
+										});
+									},
+								}}
+							/>
+							<KolSelect
+								_label="Select"
+								_orientation="horizontal"
+								_options={[
+									{ label: 'New York', value: 'New York' },
+									{ label: 'Rio de Janeiro', value: 'Rio de Janeiro' },
+									{ label: 'Rosenheim', value: 'Rosenheim' },
+								]}
+								_value={values.select}
+								_name="select"
+								_on={{
+									onInput: (event, value) => {
+										setValues({
+											...values,
+											select: value,
+										});
+									},
+								}}
+							/>
+							<KolTextarea _label="Textarea" _value={values.textarea} _name="textarea" _on={{ onInput: handleChange }} />
+							<div className="mt">
+								<KolButton _label="Button" _value={values.button} _name="button" _type="submit" />
+								<KolButtonLink _label="ButtonLink" _value={values.buttonLink} _name="buttonLink" _type="submit" className="ml" />
+							</div>
 
-						<KolButton className={'mt'} _label={'Submit'} _type={'submit'}></KolButton>
+							<KolButton className="mt w-fit" _label="Submit" _type="submit"></KolButton>
+						</div>
 					</KolForm>
 				)}
 			</Formik>
