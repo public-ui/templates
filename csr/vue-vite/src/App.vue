b936ecb4913b74d017a5d229a1c8df46f8317d24
Martin Oppitz <6279703+deleonio@users.noreply.github.com>
2025-04-17 03:35:32 +0200

    fix: update alert component attributes for consistency across templates

--- csr/vue-vite/src/App.vue
+++ csr/vue-vite/src/App.vue
@@ -12,22 +12,22 @@
 			<strong class="text-2xl">Willkommen zu KoliBri</strong>
 		</header>
 		<main class="grid md:grid-cols-2 gap-6 p-12">
-			<KolAlert :_type="'success'" _heading="Dokumentation" _variant="card" :_level="2">
+			<KolAlert :_type="'success'" _label="Dokumentation" _variant="card" :_level="2">
 				<KolLink _href="https://public-ui.github.io/" _label="" _target="_blank">
 					<KolIcon _label="" _icons="fa-sharp fa-solid fa-book" /> Dokumentation öffnen
 				</KolLink>
 			</KolAlert>
-			<KolAlert :_type="'info'" _heading="Theming" _variant="card" :_level="2">
+			<KolAlert :_type="'info'" _label="Theming" _variant="card" :_level="2">
 				<KolLink _href="https://public-ui.github.io/docs/concepts/styling/designer" _label="" _target="_blank">
 					<KolIcon _label="" _icons="fa-solid fa-palette" /> KoliBri-Designer öffnen
 				</KolLink>
 			</KolAlert>
-			<KolAlert :_type="'warning'" _heading="Mitwirken" _variant="card" :_level="2">
+			<KolAlert :_type="'warning'" _label="Mitwirken" _variant="card" :_level="2">
 				<KolLink _href="https://github.com/public-ui/kolibri/" _label="" _target="_blank">
 					<KolIcon _label="" _icons="fa-brands fa-github" /> GitHub öffnen
 				</KolLink>
 			</KolAlert>
-			<KolAlert :_type="'error'" _heading="Kontakt" _variant="card" :_level="2">
+			<KolAlert :_type="'error'" _label="Kontakt" _variant="card" :_level="2">
 				<KolLink _href="mailto:kolibri@itzbund.de" _label="" _target="_blank"> <KolIcon _label="" _icons="fa-solid fa-envelope" /> E-Mail schreiben </KolLink>
 			</KolAlert>
 		</main>
--- ssr/next.js/src/pages/index.tsx
+++ ssr/next.js/src/pages/index.tsx
@@ -36,24 +36,24 @@ const Index = () => {
 					<strong className="text-2xl">Willkommen zu KoliBri</strong>
 				</header>
 				<main className="grid md:grid-cols-2 gap-6 p-12">
-					<KolAlert _type="success" _heading="Dokumentation" _variant="card" _level={2}>
+					<KolAlert _type="success" _label="Dokumentation" _variant="card" _level={2}>
 						<KolLink _href="https://public-ui.github.io/" _label="" _target="_blank">
-							<KolIcon _ariaLabel="" _icon="fa-sharp fa-solid fa-book" /> Dokumentation öffnen
+							<KolIcon _label="" _icons="fa-sharp fa-solid fa-book" /> Dokumentation öffnen
 						</KolLink>
 					</KolAlert>
-					<KolAlert _type="info" _heading="Theming" _variant="card" _level={2}>
+					<KolAlert _type="info" _label="Theming" _variant="card" _level={2}>
 						<KolLink _href="https://public-ui.github.io/docs/concepts/styling/designer" _label="" _target="_blank">
-							<KolIcon _ariaLabel="" _icon="fa-solid fa-palette" /> KoliBri-Designer öffnen
+							<KolIcon _label="" _icons="fa-solid fa-palette" /> KoliBri-Designer öffnen
 						</KolLink>
 					</KolAlert>
-					<KolAlert _type="warning" _heading="Mitwirken" _variant="card" _level={2}>
+					<KolAlert _type="warning" _label="Mitwirken" _variant="card" _level={2}>
 						<KolLink _href="https://github.com/public-ui/kolibri/" _label="" _target="_blank">
-							<KolIcon _ariaLabel="" _icon="fa-brands fa-github" /> GitHub öffnen
+							<KolIcon _label="" _icons="fa-brands fa-github" /> GitHub öffnen
 						</KolLink>
 					</KolAlert>
-					<KolAlert _type="error" _heading="Kontakt" _variant="card" _level={2}>
+					<KolAlert _type="error" _label="Kontakt" _variant="card" _level={2}>
 						<KolLink _href="mailto:kolibri@itzbund.de" _label="" _target="_blank">
-							<KolIcon _ariaLabel="" _icon="fa-solid fa-envelope" /> E-Mail schreiben
+							<KolIcon _label="" _icons="fa-solid fa-envelope" /> E-Mail schreiben
 						</KolLink>
 					</KolAlert>
 				</main>
